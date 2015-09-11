7167af10f9d811590fb72351fe0f5ea0fe66bcb4
Peter Golian <pgo@eea.sk>
2015-09-11 10:18:22 +0200

    Missing keys added. Resolves #6

--- src/test/java/eu/comsode/unifiedviews/plugins/extractor/skgovcrz/SkGovCRZTest.java
+++ src/test/java/eu/comsode/unifiedviews/plugins/extractor/skgovcrz/SkGovCRZTest.java
@@ -1,13 +1,21 @@
 package eu.comsode.unifiedviews.plugins.extractor.skgovcrz;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
 import java.nio.charset.CodingErrorAction;
 
 import org.junit.Test;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.Rio;
 
 import cz.cuni.mff.xrg.odcs.dpu.test.TestEnvironment;
 import eu.unifiedviews.dataunit.rdf.WritableRDFDataUnit;
+import eu.unifiedviews.helpers.dataunit.rdf.RDFHelper;
 import eu.unifiedviews.helpers.dpu.test.config.ConfigurationBuilder;
 
 public class SkGovCRZTest {
@@ -24,6 +32,7 @@ public class SkGovCRZTest {
         CharsetEncoder encoder = Charset.forName("UTF-8").newEncoder();
         encoder.onMalformedInput(CodingErrorAction.REPORT);
         encoder.onUnmappableCharacter(CodingErrorAction.REPORT);
+        PrintWriter outputWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream("contracts.txt", false), encoder)));
 
         // Prepare data unit.
         WritableRDFDataUnit rdfOutput = environment.createRdfOutput("rdfOutput", false);
@@ -31,6 +40,8 @@ public class SkGovCRZTest {
             // Run.
             environment.run(dpu);
 
+            RepositoryConnection con = rdfOutput.getConnection();
+            con.export(Rio.createWriter(RDFFormat.TURTLE, outputWriter), RDFHelper.getGraphsURIArray(rdfOutput));
         } finally {
             // Release resources.
             environment.release();
